81abd61c5615e364ec01bfa4789fcf9765bacbe5
Ciprian <48862508+Sufferal@users.noreply.github.com>
2024-06-18 23:02:50 +0300

    feat: add delete task (#3)

---
+++ front-end/do-it-now/src/components/Task/Actions/TaskDelete.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-btn
+    class="task-delete-btn"
+    icon="mdi-close-circle"
+    @click="dialog = true"
+  ></v-btn>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card class="delete-card">
+      <v-card-title class="headline"
+        >Are you sure you want to delete this task?</v-card-title
+      >
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn class="delete-btn" variant="flat" @click="deleteTaskHandler"
+          >Delete</v-btn
+        >
+        <v-btn class="cancel-btn" variant="outlined" @click="dialog = false"
+          >Cancel</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    deleteTask: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup(props) {
+    const dialog = ref(false);
+    const deleteTaskHandler = () => {
+      dialog.value = false;
+      props.deleteTask();
+    };
+    return { dialog, deleteTaskHandler };
+  },
+});
+</script>
+
+<style scoped>
+.task-delete-btn {
+  background-color: var(--error-color);
+}
+.delete-card:deep() .headline {
+  color: var(--background-color) !important;
+}
+.delete-btn {
+  background-color: var(--error-color);
+}
+.cancel-btn {
+  color: var(--secondary-color);
+}
+.delete-btn:deep() .v-btn__content {
+  color: var(--primary-color);
+  font-weight: bold;
+}
+.cancel-btn:deep() .v-btn__content {
+  color: var(--secondary-color);
+  font-weight: bold;
+}
+</style>
