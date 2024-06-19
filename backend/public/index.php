6ae84015537b4dbb9824ef4da6a03476e81ad8aa
Ciprian <48862508+Sufferal@users.noreply.github.com>
2024-06-19 13:00:10 +0300

    feat: add DB connection (#7)

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,3 +1,6 @@
 <?php
-  echo 'Hello World';
-?>
+require_once '../utils/Database.php';
+require_once '../config/config.php';
+
+$database = new Database($host, $db, $user, $pass, $charset);
+$database->create_tasks_table();
